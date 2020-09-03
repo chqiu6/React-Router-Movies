28d60b1b1548059b175866d5404fedeed65748af
chqiu6 <chucky-qiu@lambdastudents.com>
2020-09-03 19:27:09 -0400

    somehow got the route path of movielist to work n mvp

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import MovieList from "./Movies/MovieList";
 import {Route} from "react-router-dom";
+import Movie from "./Movies/Movie";
 
 import SavedList from './Movies/SavedList';
 
@@ -32,9 +33,15 @@ const App = () => {
     <div>
       <SavedList list={[ /* This is stretch */]} />
       <div>Replace this Div with your Routes</div>
+      {/* one route for `/` that loads the `MovieList` component. **This component will need the movies injected into it via props**. */}
       <Route exact path="/">
-        <MovieList movie = {MovieList}/>
+        <MovieList movies = {movieList}/>
       </Route>
+      {/* one route that will take an `id` parameter after`/movies/` (ex: `/movies/2`, `/movies/3` where the id is dynamic). This route should load the `Movie` component. */}
+    {/* <Route exact path = "/movies/:id" component = {Movie} /> */}
+    <Route exact path = "/movies/:id">
+      <Movie movie = {Movie}/>
+    </Route>
     </div>
   );
 };
